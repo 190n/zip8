4514efb6200ab12dfcb4a9544498e7d7bdb8147a
Ben Grant <benjamin.j.grant@gmail.com>
2023-09-10 15:30:23 -0700

    Rewrite web version in TypeScript

---
+++ web-host/src/cpu.ts
@@ -0,0 +1,200 @@
+import wasmUrl from '../../zig-out/lib/zip8.wasm?url';
+
+interface Zip8Exports {
+	memory: WebAssembly.Memory;
+	ZIP8_ERR_ILLEGAL_OPCODE: WebAssembly.Global;
+	ZIP8_ERR_STACK_OVERFLOW: WebAssembly.Global;
+	ZIP8_ERR_BAD_RETURN: WebAssembly.Global;
+	ZIP8_ERR_PROGRAM_TOO_LONG: WebAssembly.Global;
+	ZIP8_ERR_FLAG_OVERFLOW: WebAssembly.Global;
+
+	zip8GetErrorName(err: number): number;
+	zip8CpuGetSize(): number;
+	zip8CpuInit(
+		errPtr: number,
+		cpuPtr: number,
+		programBuf: number,
+		programLen: number,
+		seed: bigint,
+		flags: bigint
+	): number;
+	zip8CpuCycle(errPtr: number, cpuPtr: number): number;
+	zip8CpuSetKeys(cpuPtr: number, keys: number): void;
+	zip8CpuIsWaitingForKey(cpuPtr: number): boolean;
+	zip8CpuTimerTick(cpuPtr: number): void;
+	zip8CpuDisplayIsDirty(cpuPtr: number): boolean;
+	zip8CpuSetDisplayNotDirty(cpuPtr: number): void;
+	zip8CpuGetDisplay(cpuPtr: number): number;
+	zip8CpuGetInstruction(cpuPtr: number): number;
+	zip8CpuGetProgramCounter(cpuPtr: number): number;
+	zip8CpuGetFlags(cpuPtr: number): bigint;
+	zip8CpuFlagsAreDirty(cpuPtr: number): boolean;
+	zip8CpuSetFlagsNotDirty(cpuPtr: number): void;
+	zip8CpuAlloc(): number;
+	wasmAlloc(size: number): number;
+}
+
+function decodeNullTerminatedString(memory: WebAssembly.Memory, start: number) {
+	const view = new Uint8Array(memory.buffer);
+	let length = 0;
+	while (view[start + length] != 0) {
+		length += 1;
+	}
+	return new TextDecoder('utf-8').decode(
+		memory.buffer.slice(start, start + length)
+	);
+}
+
+export default class CPU {
+	static wasmModule: WebAssembly.Module | null = null;
+
+	private exports: Zip8Exports = {} as any;
+	private errPtr: number = 0;
+	private cpuPtr: number = 0;
+	private display: Uint8Array = new Uint8Array();
+
+	private constructor() {}
+
+	private logCallback(pointer: number, size: number) {
+		const string = new TextDecoder('utf-8').decode(
+			this.exports.memory.buffer.slice(pointer, pointer + size)
+		);
+		console.log(string);
+	}
+
+	private fallibleCall(result: number) {
+		if (result != 0) {
+			const err = new DataView(this.exports.memory.buffer).getUint16(
+				this.errPtr,
+				true
+			);
+			const stringPointer = this.exports.zip8GetErrorName(err);
+			throw new Error(
+				decodeNullTerminatedString(this.exports.memory, stringPointer)
+			);
+		}
+	}
+
+	static async init(
+		program: ArrayBuffer,
+		seed: bigint,
+		initialFlags: number[]
+	): Promise<CPU> {
+		const cpu = new CPU();
+
+		if (CPU.wasmModule == null) {
+			CPU.wasmModule = await WebAssembly.compileStreaming(fetch(wasmUrl));
+		}
+		cpu.exports = (
+			await WebAssembly.instantiate(CPU.wasmModule, {
+				env: {
+					zip8Log: cpu.logCallback.bind(cpu),
+				},
+			})
+		).exports as unknown as Zip8Exports;
+		cpu.errPtr = cpu.exports.wasmAlloc(2);
+		cpu.cpuPtr = cpu.exports.zip8CpuAlloc();
+		const programBuf = cpu.exports.wasmAlloc(program.byteLength);
+		new Uint8Array(cpu.exports.memory.buffer).set(
+			new Uint8Array(program),
+			programBuf
+		);
+
+		let flagsNum = 0n;
+		for (let i = 0; i < 8; i++) {
+			flagsNum |= BigInt(initialFlags[i]) << BigInt(8 * i);
+		}
+		cpu.fallibleCall(
+			cpu.exports.zip8CpuInit(
+				cpu.errPtr,
+				cpu.cpuPtr,
+				programBuf,
+				program.byteLength,
+				seed,
+				flagsNum
+			)
+		);
+
+		const displayPtr = cpu.exports.zip8CpuGetDisplay(cpu.cpuPtr);
+		cpu.display = new Uint8Array(
+			cpu.exports.memory.buffer,
+			displayPtr,
+			(64 * 32) / 8
+		);
+		return cpu;
+	}
+
+	cycle() {
+		this.fallibleCall(this.exports.zip8CpuCycle(this.errPtr, this.cpuPtr));
+	}
+
+	timerTick() {
+		this.exports.zip8CpuTimerTick(this.cpuPtr);
+	}
+
+	getPixel(x: number, y: number): boolean {
+		const index = 64 * y + x;
+		const byteIndex = Math.floor(index / 8);
+		const bitIndex = index % 8;
+		return Boolean((this.display[byteIndex] >> bitIndex) & 1);
+	}
+
+	renderDisplay(out: ImageData) {
+		for (let y = 0; y < 32; y += 1) {
+			for (let x = 0; x < 64; x += 1) {
+				const pixel = this.getPixel(x, y) ? 255 : 0;
+				out.data[(y * 64 + x) * 4 + 0] = pixel;
+				out.data[(y * 64 + x) * 4 + 1] = pixel;
+				out.data[(y * 64 + x) * 4 + 2] = pixel;
+				out.data[(y * 64 + x) * 4 + 3] = 255;
+			}
+		}
+	}
+
+	setKeys(keys: boolean[]) {
+		let keysNum = 0;
+		for (let i = 0; i < 16; i++) {
+			if (keys[i]) {
+				keysNum |= 1 << i;
+			}
+		}
+		this.exports.zip8CpuSetKeys(this.cpuPtr, keysNum);
+	}
+
+	isWaitingForKey(): boolean {
+		return this.exports.zip8CpuIsWaitingForKey(this.cpuPtr);
+	}
+
+	displayIsDirty(): boolean {
+		return this.exports.zip8CpuDisplayIsDirty(this.cpuPtr);
+	}
+
+	setDisplayNotDirty() {
+		this.exports.zip8CpuSetDisplayNotDirty(this.cpuPtr);
+	}
+
+	getInstruction(): number {
+		return this.exports.zip8CpuGetInstruction(this.cpuPtr);
+	}
+
+	getProgramCounter(): number {
+		return this.exports.zip8CpuGetProgramCounter(this.cpuPtr);
+	}
+
+	flagsAreDirty(): boolean {
+		return this.exports.zip8CpuFlagsAreDirty(this.cpuPtr);
+	}
+
+	setFlagsNotDirty() {
+		this.exports.zip8CpuSetFlagsNotDirty(this.cpuPtr);
+	}
+
+	getFlags(): number[] {
+		const flags = Array(8).fill(0);
+		const flagsNum = this.exports.zip8CpuGetFlags(this.cpuPtr);
+		for (let i = 0; i < 8; i++) {
+			flags[i] = Number(flagsNum >> BigInt(8 * i)) & 0xff;
+		}
+		return flags;
+	}
+}
---
+++ web-host/src/main.ts
@@ -0,0 +1,133 @@
+import CPU from './cpu.ts';
+
+const canvas = document.getElementById('canvas')! as HTMLCanvasElement;
+const ctx = canvas.getContext('2d')!;
+const output = document.getElementById('output')!;
+
+const imageData = ctx.createImageData(64, 32);
+
+const keys = Array(16).fill(false);
+
+const keyBindings = {
+	'1': 0x1,
+	'2': 0x2,
+	'3': 0x3,
+	'4': 0xc,
+	q: 0x4,
+	w: 0x5,
+	e: 0x6,
+	r: 0xd,
+	a: 0x7,
+	s: 0x8,
+	d: 0x9,
+	f: 0xe,
+	z: 0xa,
+	x: 0x0,
+	c: 0xb,
+	v: 0xf,
+};
+
+window.onkeydown = (e) => {
+	if (e.key in keyBindings) {
+		keys[keyBindings[e.key as keyof typeof keyBindings]] = true;
+	}
+};
+
+window.onkeyup = (e) => {
+	if (e.key in keyBindings) {
+		keys[keyBindings[e.key as keyof typeof keyBindings]] = false;
+	}
+};
+
+window.ontouchstart = (e: TouchEvent) => {
+	const tagName = (e.target as HTMLElement).tagName;
+	if (tagName == 'A' || tagName == 'BUTTON' || tagName == 'INPUT') return true;
+	e.preventDefault();
+	keys.fill(true);
+};
+
+window.ontouchend = (e: TouchEvent) => {
+	const tagName = (e.target as HTMLElement).tagName;
+	if (tagName == 'A' || tagName == 'BUTTON' || tagName == 'INPUT') return true;
+	e.preventDefault();
+	keys.fill(false);
+};
+
+let timeout: number | null = null;
+
+async function run(rom: ArrayBuffer) {
+	if (timeout !== null) clearTimeout(timeout);
+
+	const key = 'zip8Flags';
+	const initialFlags =
+		localStorage.getItem(key) === null
+			? Array(8).fill(0)
+			: JSON.parse(localStorage.getItem(key)!);
+
+	output.innerHTML = '';
+
+	const cpu = await CPU.init(
+		rom,
+		BigInt(Math.floor(Math.random() * 1000000)),
+		initialFlags
+	);
+
+	const instructionsPerTick = 200;
+
+	let halt = false;
+
+	function tick() {
+		if (halt) return;
+		timeout = setTimeout(tick, 1000 / 60);
+		cpu.setKeys(keys);
+
+		if (!cpu.isWaitingForKey()) {
+			for (let i = 0; i < instructionsPerTick && !halt; i++) {
+				try {
+					cpu.cycle();
+				} catch (e) {
+					halt = true;
+					output.innerHTML = `Error: ${(e as Error).message}<br>
+						at 0x${cpu.getProgramCounter().toString(16).padStart(3, '0')},
+						instruction 0x${cpu.getInstruction().toString(16).padStart(4, '0')}`;
+				}
+			}
+
+			cpu.timerTick();
+			if (cpu.displayIsDirty()) {
+				cpu.renderDisplay(imageData);
+				ctx.putImageData(imageData, 0, 0);
+				cpu.setDisplayNotDirty();
+			}
+
+			if (cpu.flagsAreDirty()) {
+				const flags = cpu.getFlags();
+				localStorage.setItem(key, JSON.stringify(flags));
+			}
+		}
+	}
+
+	tick();
+}
+
+async function loadRom(this: HTMLElement) {
+	const url = this.dataset.romUrl as string;
+	run(await (await fetch(url)).arrayBuffer());
+}
+
+for (const e of document.getElementsByClassName('rom-loader')) {
+	e.addEventListener('click', loadRom, false);
+}
+
+document.getElementById('upload')!.onchange = (e: Event) => {
+	const file = (e.target as HTMLInputElement).files![0];
+	const reader = new FileReader();
+	reader.onload = () => {
+		run(reader.result as ArrayBuffer);
+	};
+	reader.readAsArrayBuffer(file);
+};
+
+(async () => {
+	run(await (await fetch('flappybird.ch8')).arrayBuffer());
+})();
